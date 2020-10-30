773da25f9aa362343ef38a3c7363d729db194e48
Nilesh <cnilesh97@gmail.com>
2020-10-30 13:09:45 +0530

    Encode password

--- src/main/java/com/skodoskxx/techie/springngblog/model/User.java
+++ src/main/java/com/skodoskxx/techie/springngblog/model/User.java
@@ -23,35 +23,31 @@ public class User {
         return id;
     }
 
-    public User setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
-        return this;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public User setUserName(String userName) {
+    public void setUserName(String userName) {
         this.userName = userName;
-        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public User setPassword(String password) {
+    public void setPassword(String password) {
         this.password = password;
-        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public User setEmail(String email) {
+    public void setEmail(String email) {
         this.email = email;
-        return this;
     }
 }
--- src/main/java/com/skodoskxx/techie/springngblog/service/AuthService.java
+++ src/main/java/com/skodoskxx/techie/springngblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.skodoskxx.techie.springngblog.dto.RegisterRequest;
 import com.skodoskxx.techie.springngblog.model.User;
 import com.skodoskxx.techie.springngblog.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,18 @@ public class AuthService {
 
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepo.save(user);
+    }
+
+    private String encodePassword(String password){
+        return passwordEncoder.encode(password);
     }
 }
